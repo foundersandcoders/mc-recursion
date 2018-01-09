b50d0182c277e6c96ef648956e54f902e914e6f4
Zooey <zooeyxmiller@gmail.com>
2018-01-09 09:06:22 +0000

    changes reduce to mirror js more changes function to take (acc, el), changes type signature, changes tests to reflect changes. #4 #3

--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -2,17 +2,22 @@ const { reduce } = require('../index');
 
 describe('reduce', () => {
   it('can be used to sum an array', () => {
-    expect(reduce((a, b) => a + b, 0, [1,2,3,4])).toEqual(10);
+    expect(reduce((a, b) => a + b, 0, [1, 2, 3, 4])).toEqual(10);
   });
 
   it('can be used to filter an array', () => {
-    expect(reduce((a, b) => {
-      if (a.length === 3) {
-        return [a].concat(b)
-      } else {
-        return b
-      }
-    }, [], ["pumpkin", "pug", "po", "pof"]))
-      .toEqual(['pug', 'pof']);
+    expect(
+      reduce(
+        (a, b) => {
+          if (b.length === 3) {
+            return a.concat([b]);
+          } else {
+            return a;
+          }
+        },
+        [],
+        ['pumpkin', 'pug', 'po', 'pof']
+      )
+    ).toEqual(['pug', 'pof']);
   });
 });
--- tests/reduceMap.test.js
+++ tests/reduceMap.test.js
@@ -2,12 +2,14 @@ const { reduceMap } = require('../index');
 
 describe('map', () => {
   it('Can run add a number to all items in an array', () => {
-    expect(reduceMap(x => x + 1, [0,3,4]))
-      .toEqual([1,4,5]);
+    expect(reduceMap(x => x + 1, [0, 3, 4])).toEqual([1, 4, 5]);
   });
 
   it('Can uppercase all strings in an array', () => {
-    expect(reduceMap(x => x.toUpperCase(), ['the', 'moon', 'is']))
-      .toEqual(['THE', 'MOON', 'IS']);
+    expect(reduceMap(x => x.toUpperCase(), ['the', 'moon', 'is'])).toEqual([
+      'THE',
+      'MOON',
+      'IS'
+    ]);
   });
 });
